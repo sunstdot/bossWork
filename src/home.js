72697f4307875a03cad1b0f0f09f054da4dcbd4c
sunshitao <sunshitao@qiyi.com>
2017-10-19 18:18:23 +0800

    test

--- src/home.js
+++ src/home.js
@@ -1,11 +1,42 @@
 (function ($){
 
     bindEvent() {
-        var headNav = $('*[data-elem="nav"]');
-        headNav.on('mouseenter', function() {
 
+    }
+    initProductHover() {
+        var productCenter = $('#productCenter');
+        var productDoms =  productCenter.down('*[data-elem-item="product"]');
+        var firstDom= $(productDoms[0]);
+        var selectDom = firstDom;
+        productCenter.on('mouseenter', function(e) {
+            var parentDom;
+            var target = $(e.target || e.srcElement);
+            while(target.attr('data-elem-item') !== 'product')  {
+              target = target.parentNode();
+            }
+            if(selectDom && selectDom.hasClass('on')) {
+              selectDom.removeClass('on');
+            }
+            if(!target.hasClass('on')) {
+                target.addClass('on');
+            }
+        });
+        productCenter.on('mouseleave', function(e) {
+            if(!firstDom.hasClass('on')) {
+                firstDom.addClass('on');
+            }
+            if(selectDom.hasClass('on')) {
+                selectDom.removeClass('on');
+            }
         });
     }
+
+    initFocus() {
+        
+    }
+
+
+
     function init() {
         bindEvent();
         initScroll(); //初始化全屏滚动事件
