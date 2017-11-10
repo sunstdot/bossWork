6246596125d12769bf7b99f88eeeda48351b03f1
sunshitao <sunshitao@qiyi.com>
2017-11-10 20:08:49 +0800

    test

--- src/home.js
+++ src/home.js
@@ -1,47 +1,75 @@
 (function ($){
 
-    bindEvent() {
+    //初始化事件绑定
+    function bindEvent() {
 
     }
-    initProductHover() {
+    //初始化滚动事件
+    function initScroll() {
+        $('#fullpage').fullpage();
+    }
+    //初始化hover事件
+    function initProductHover() {
         var productCenter = $('#productCenter');
-        var productDoms =  productCenter.down('*[data-elem-item="product"]');
+        var productDoms =  productCenter.find('*[data-elem-item="product"]');
         var firstDom= $(productDoms[0]);
         var selectDom = firstDom;
-        productCenter.on('mouseenter', function(e) {
+        productDoms.on('mouseenter', function(e) {
             var parentDom;
             var target = $(e.target || e.srcElement);
-            while(target.attr('data-elem-item') !== 'product')  {
-              target = target.parentNode();
-            }
+            // while(target.attr('data-elem-item') !== 'product')  {
+              target = target.parents('*[data-elem-item="product"]');
+            // }
             if(selectDom && selectDom.hasClass('on')) {
               selectDom.removeClass('on');
             }
             if(!target.hasClass('on')) {
                 target.addClass('on');
             }
+            selectDom = target;
         });
-        productCenter.on('mouseleave', function(e) {
-            if(!firstDom.hasClass('on')) {
-                firstDom.addClass('on');
-            }
+        productDoms.on('mouseleave', function(e) {
             if(selectDom.hasClass('on')) {
                 selectDom.removeClass('on');
             }
+            selectDom = firstDom;
+            if(!selectDom.hasClass('on')) {
+                selectDom.addClass('on');
+            }
         });
     }
+    //初始化焦点图事件
+    function initFocus() {
+        var slicebox = $('#sb-slider').slicebox({
+           onReady: function() {
+               console.log('------ready');
+           },
+           orientation : 'r',
+					 cuboidsRandom : true,
+					 disperseFactor : 30
+        })
 
-    initFocus() {
-        
+        var leftBtn = $('#leftArrowBtn');
+        var rightBtn = $('#rightArrowBtn');
+        leftBtn.on('click', function(e) {
+            console.log('--------------left');
+            slicebox.previous();
+						return false;
+        });
+        rightBtn.on('click', function(e) {
+            console.log('--------------right');
+            slicebox.next();
+						return false;
+        });
     }
 
 
 
     function init() {
-        bindEvent();
+        // bindEvent();
         initScroll(); //初始化全屏滚动事件
         initFocus();  //初始化焦点图事件
         initProductHover();   //初始化产品图hover事件
     }
     init();
-})(jquery)
+})($)
